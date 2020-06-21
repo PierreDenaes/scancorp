cb5d18c889efd9815b2094581ff4afd5d057502d
Dnadapierre <photostudio13000@gmail.com>
2020-06-21 18:04:57 +0200

    update phpmailer

--- inc/vendor/composer/autoload_real.php
+++ inc/vendor/composer/autoload_real.php
@@ -13,6 +13,9 @@ class ComposerAutoloaderInitd1ca102aae01d7b9787fb82b1ecda9a8
         }
     }
 
+    /**
+     * @return \Composer\Autoload\ClassLoader
+     */
     public static function getLoader()
     {
         if (null !== self::$loader) {
